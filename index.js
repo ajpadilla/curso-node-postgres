f635e152249eaa81f8a24bb9d45490bd7f6cb511
Alvaro Padilla <ajpadilla88@gmail.com>
2026-01-13 10:26:27 -0500

    Updatind index file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
-const {checkApiKey} = require('./middlewares/auth.handler');
+const {checkApiKey} = require('./middlewares/auth.middleware');
 
-const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.middleware');
 
 const app = express();
 const port = process.env.PORT || 3000;
