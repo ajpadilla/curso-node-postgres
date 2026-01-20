6ce2da41898a15a13be63732fa8d60a61ea87e7f
Alvaro Padilla <ajpadilla88@gmail.com>
2026-01-20 09:43:43 -0500

    Set up toolkit componets

---
+++ test/toolkit/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  InMemoryRepository: require('./fakes/in-memory.repository'),
+  UserBuilder: require('./builders/user.builder'),
+  usersFixture: require('./fixtures/users.fixture'),
+};
