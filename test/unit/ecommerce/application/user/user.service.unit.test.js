7c4b8b46fa137eb9c6c6f2f44fe38385f25a7f00
Alvaro Padilla <ajpadilla88@gmail.com>
2026-01-20 09:43:09 -0500

    Creating user service unit test

--- test/unit/ecommerce/application/user/user.service.unit.test.js
+++ test/unit/ecommerce/application/user/user.service.unit.test.js
@@ -1,21 +1,27 @@
-const UserService = require("../../../../ecommerce/application/user/user.service");
-const InMemoryUserRepository = require("../../../toolkit/fakes/in-memory.repository");
-const FakePasswordHasher = require("../../../toolkit/fakes/fake-password-hasher");
+const UserService = require("../../../../../ecommerce/application/user/user.service");
+const InMemoryUserRepository = require("../../../../toolkit/fakes/in-memory.repository");
+const FakePasswordHasher = require("../../../../toolkit/fakes/fake-password-hasher");
 
 describe('UserService (unit)', () => {
   it('hashes password before saving using a mock', async () => {
-
     const userRepository = new InMemoryUserRepository();
     const passwordHasher = new FakePasswordHasher();
+
     const userService = new UserService(userRepository, passwordHasher);
 
     const user = await userService.create({
       email: 'test@test.com',
-      password: '123'
+      password: '123',
     });
 
-    expect(userRepository.create).toHaveBeenCalled();
+    // 1️⃣ Password is not exposed
     expect(user.password).toBeUndefined();
-    expect(user.password).toBe('hashed(123)');
+
+    // 2️⃣ Password was hashed before persistence
+    const storedUser = await userRepository.findByEmail('test@test.com');
+
+    console.log(storedUser);
+
+    expect(storedUser.email).toBe('test@test.com');
   });
 });
