f15aaff9dff88edb9150f5396cd52c53dee55856
Alvaro Padilla <ajpadilla88@gmail.com>
2026-02-02 21:17:51 -0500

    Updating app file

--- app.js
+++ app.js
@@ -4,38 +4,59 @@ const cors = require('cors');
 const routerApi = require('./ecommerce/bootstrap/container');
 const { checkApiKey } = require('./ecommerce/infrastructure/http/middlewares/auth.middleware');
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./ecommerce/infrastructure/http/middlewares/error.middleware');
+const {join} = require("path");
+const expressLayouts = require('express-ejs-layouts');
+const cookieParser = require("cookie-parser");
 
-require('./utils/auth'); // passport strategies
 
-const app = express();
-app.use(express.json());
+function buildApp() {
+  const app = express();
 
-// CORS setup
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('no permitido'));
+  // Views config
+  app.set('view engine', 'ejs');
+  app.set('views', join(__dirname, 'views'));
+  app.use('/js', express.static(join(__dirname, 'frontend/js')));
+
+  // Layouts
+  app.use(expressLayouts);
+  app.set('layout', 'layouts/app');
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:8080',
+    'https://myapp.co'
+  ];
+
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('no permitido'));
+      }
     }
-  }
-};
-app.use(cors(corsOptions));
+  };
 
-// Simple root route
-app.get('/', (req, res) => res.send('Hola mi server en express'));
+  app.use(cors(corsOptions));
+  app.use(cookieParser());
 
-// Protected route example
-app.get('/nueva-ruta', checkApiKey, (req, res) => res.send('Hola, soy una nueva ruta'));
+  app.get('/', (req, res) => res.send('Hola mi server en express'));
 
-// Attach domain routes
-routerApi(app);
+  app.get('/nueva-ruta', checkApiKey, (req, res) =>
+    res.send('Hola, soy una nueva ruta')
+  );
 
-// Error middlewares
-app.use(logErrors);
-app.use(ormErrorHandler);
-app.use(boomErrorHandler);
-app.use(errorHandler);
+  routerApi(app);
 
-module.exports = app;
+  app.use(logErrors);
+  app.use(ormErrorHandler);
+  app.use(boomErrorHandler);
+  app.use(errorHandler);
+
+  return app;
+}
+
+module.exports = buildApp;
