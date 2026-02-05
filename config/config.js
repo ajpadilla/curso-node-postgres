214aa983309daefc376b1d18d0b35bbaab9d53c8
Alvaro Padilla <ajpadilla88@gmail.com>
2026-02-05 17:44:07 -0500

    Adding config files

--- config/config.js
+++ config/config.js
@@ -1,26 +1,30 @@
-{
-  "development": {
-    "username": "nico",
-    "password": "admin123",
-    "database": "my_store",
-    "host": "localhost",
-    "port": 5532,
-    "dialect": "postgres"
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
   },
-  "test": {
-    "username": "nico",
-    "password": "admin123",
-    "database": "my_store",
-    "host": "localhost",
-    "port": 5532,
-    "dialect": "postgres"
+
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME + '_test',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
   },
-  "production": {
-    "username": "nico",
-    "password": "admin123",
-    "database": "my_store",
-    "host": "localhost",
-    "port": 5532,
-    "dialect": "postgres"
-  }
-}
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+};
--- config/env.js
+++ config/env.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 
 const env = {
-  env: process.env.NODE_ENV || 'dev',
-  dbHost: process.env.DB_HOST,
-  port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dhHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
-  apiKey: process.env.API_KEY,
-  jwtSecret: process.env.JWT_SECRET
-}
+  env: process.env.NODE_ENV || 'development',
 
-module.exports = {
-  config: env
+  port: process.env.PORT || 3000,
+
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5532,
+    user: process.env.DB_USER || 'nico',
+    password: process.env.DB_PASSWORD || 'admin123',
+    name: process.env.DB_NAME || 'my_store',
+  },
+
+  apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
 };
+
+module.exports = { config: env };
