461f4616e5e7a44b743d134361163e8990b14cf8
Alvaro Padilla <ajpadilla88@gmail.com>
2026-02-05 17:36:57 -0500

    Adding migration folder

---
+++ database/migrations/20260204153254-create-users-table.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      recovery_token: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'customer',
+      },
+
+      create_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users');
+  },
+};
