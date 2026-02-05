f063792b5401bb5ffb797ee5b08f00a6fee80feb
Alvaro Padilla <ajpadilla88@gmail.com>
2026-02-05 17:36:11 -0500

    Adding playwright.config.js

---
+++ ecommerce/infrastructure/logger/WinstonLogger.js
@@ -0,0 +1,44 @@
+const Logger = require('../../../shared/logger/Logger');
+const { createLogger, format, transports } = require('winston');
+
+class WinstonLogger extends Logger {
+  constructor() {
+    super();
+    this.logger = createLogger({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      format: format.combine(
+        format.timestamp(),
+        format.errors({ stack: true }), // Include stack traces
+        format.splat(),
+        format.json() // JSON for production
+      ),
+      defaultMeta: { service: 'my-express-app' },
+      transports: [
+        new transports.Console({
+          format:
+            process.env.NODE_ENV === 'production'
+              ? format.json()
+              : format.combine(format.colorize(), format.simple())
+        }),
+      ],
+    });
+  }
+
+  info(msg, meta) {
+    this.logger.info(msg, meta);
+  }
+
+  warn(msg, meta) {
+    this.logger.warn(msg, meta);
+  }
+
+  error(msg, meta) {
+    this.logger.error(msg, meta);
+  }
+
+  debug(msg, meta) {
+    this.logger.debug(msg, meta);
+  }
+}
+
+module.exports = WinstonLogger;
---
+++ test/e2e-ui/login.spec.js
@@ -0,0 +1,22 @@
+// e2e-ui/login.spec.js
+const { test, expect } = require('@playwright/test');
+
+test.describe('Login Page', () => {
+  test('should login successfully and redirect to dashboard', async ({ page }) => {
+    // 1️⃣ Go to login page
+    await page.goto('http://localhost:3000/api/v1/auth/login');
+
+    // 2️⃣ Fill form
+    await page.fill('#email', 'user@test.com');
+    await page.fill('#password', '12345678');
+
+    // 3️⃣ Click login
+    await page.click('button[type="submit"]');
+
+    // 4️⃣ Wait for redirect
+    await page.waitForURL('**/api/v1/auth/dashboard');
+
+    // 5️⃣ Verify dashboard content
+    await expect(page).toHaveURL(/dashboard/);
+  });
+});
