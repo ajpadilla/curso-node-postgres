a22b1675f7a5cb9b357cbc7ceda800f846116cef
Alvaro Padilla <ajpadilla88@gmail.com>
2026-06-09 19:09:58 -0500

    Updatiing server.js file

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = buildApp();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0' , () => {
+app.listen(port, '0.0.0.0', () => {
   //console.log(`Server running on port ${port}`);
 });
